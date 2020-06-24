a675bdf983ef5c07818c283d226bbb1f7dfad496
ArmenMesropyan <mesropyan200315@gmail.com>
2020-06-25 01:20:01 +0400

    Header component styles and grid added

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import {Container} from 'reactstrap';
+import {Container, Row, Col} from 'reactstrap';
+import './header.scss';
 
 export default class MoviesPage extends Component {
 
@@ -7,9 +8,30 @@ export default class MoviesPage extends Component {
 
     render() {
         return (
-            <header className="movies__header">
-                Header
-                <img src="/logo.png" alt=""/>
+            <header className="main__header main-header">
+                <Container tag="nav" className="main-header__nav">
+                    <Row tag="ul" className="main-header__list">
+                        <Col tag="li" className="main-header__item main-header__item_logo">
+                            <a href="/">
+                                <img src="/logo.png" alt="Movies DB"/>
+                            </a>
+                        </Col>
+                        <Col tag="li" className="main-header__item main-header__item_links">
+                            <ul className="main-header__links">
+                                <li className="main-header__link active">
+                                    <a href="/test">
+                                        Test
+                                    </a>
+                                </li>
+                                <li className="main-header__link">
+                                    <a href="/test">
+                                        TestWO
+                                    </a>
+                                </li>
+                            </ul>
+                        </Col>
+                    </Row>
+                </Container>
             </header>
         );
     }
